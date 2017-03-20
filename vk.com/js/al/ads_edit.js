6ede5a97a224b628a4f63c571bbea48c7f95ad8b
git <git@.(none)>
2017-03-20 16:08:00 +0300

    20-03-2017 16:07:56

--- vk.com/js/al/ads_edit.js
+++ vk.com/js/al/ads_edit.js
@@ -2838,8 +2838,11 @@ AdsViewEditor.prototype.onParamUpdate = function(paramName, paramValue, forceDat
     switch (paramName) {
       case 'format_type':
         var formatPhotoSize = this.getFormatPhotoSize();
-        this.params.cost_type.cpm_only            = inArray(this.params.format_type.value, [AdsEdit.ADS_AD_FORMAT_TYPE_APPS_ONLY, AdsEdit.ADS_AD_FORMAT_TYPE_GROUPS_ONLY, AdsEdit.ADS_AD_FORMAT_TYPE_BIG_APP, AdsEdit.ADS_AD_FORMAT_TYPE_MOBILE, AdsEdit.ADS_AD_FORMAT_TYPE_PROMOTED_POST]);
-        this.params.cost_type.hidden              = this.params.cost_type.cpm_only;
+        this.params.cost_type.cpm_only = (
+          inArray(this.params.format_type.value, [AdsEdit.ADS_AD_FORMAT_TYPE_APPS_ONLY, AdsEdit.ADS_AD_FORMAT_TYPE_GROUPS_ONLY, AdsEdit.ADS_AD_FORMAT_TYPE_BIG_APP, AdsEdit.ADS_AD_FORMAT_TYPE_MOBILE])
+          || (this.params.format_type.value == AdsEdit.ADS_AD_FORMAT_TYPE_PROMOTED_POST && !this.params.cost_type.allow_promoted_posts_cpc)
+        );
+        this.params.cost_type.hidden   = this.params.cost_type.cpm_only;
 
         if (this.params.cost_type.cpm_only) {
           this.setCostType(AdsEdit.ADS_AD_COST_TYPE_VIEWS);
@@ -3478,15 +3481,6 @@ AdsViewEditor.prototype.setUpdateData = function(data, result) {
     }
 
     this.updateUiParam('cost_per_click');
-  }
-
-  if (isObject(result)) {
-    var cpm_only = result.cpm_only;
-    if (cpm_only === undefined) {
-      delete this.params.cost_type.server_cpm_only;
-    } else {
-      this.params.cost_type.server_cpm_only = cpm_only;
-    }
   }
 
   if (isObject(result) && 'audience_count_text' in result) {
