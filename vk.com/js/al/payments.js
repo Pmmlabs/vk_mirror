39ee6961b4a175f53258fb47672f318f0ac812b5
git <git@.(none)>
2018-02-06 16:08:13 +0300

    06-02-2018 16:08:09

--- vk.com/js/al/payments.js
+++ vk.com/js/al/payments.js
@@ -1010,14 +1010,14 @@ var MoneyTransfer = {
     domReplaceEl(btn, newBtn);
   },
 
-  initHistoryBox: function(oid) {
+  initHistoryBox: function(oid, request_id) {
     var btn = ge('ui_money_transfer_load_more');
     if (btn) {
       var tbl = ge('settings_transfer_history').tBodies[0];
       cur.userAutoScroll = new AutoList(tbl, {
         onNoMore: re.pbind(btn),
         onNeedRows: function(cb, offset) {
-          ajax.post('al_payments.php', { act: 'money_transfer_history_box', offset: offset, owner_id: oid }, {
+          ajax.post('al_payments.php', { act: 'money_transfer_history_box', offset: offset, owner_id: oid, request_id: request_id }, {
             onDone: cb
           });
         },
